350f97be95de38a916069e859219ffc9dc07c839
liyuqi <am997liyuqi@gmail.com>
2015-07-29 10:23:40 +0800

    first prac

--- d3_route.js
+++ d3_route.js
@@ -40,7 +40,7 @@ app.listen(app.get('port'), function(){
 
 
 
-// prac
+// prac test
 // 視覺化的夢幻組合 － D3.js 、 Adobe Illustrator 與 NodeJS
 // [[http://blog.infographics.tw/2015/04/d3js-adobe-illustrator-nodejs/]]
 var d3 = require("d3");
